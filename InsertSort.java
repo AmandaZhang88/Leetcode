3dadcc6f757a71ec5a5e87bacb78a2b83966d467
Fengnan Zhang <73869130+zhangf2atwit@users.noreply.github.com>
2021-12-23 22:15:51 -0500

    Update InsertSort.java

--- InsertSort.java
+++ InsertSort.java
@@ -3,9 +3,7 @@ package AmandaZhang88.com;
 public class Main {
 
     public static void main(String[] args) {
-
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length;
                 firstUnsortedIndex++) {
             int newElement = intArray[firstUnsortedIndex];
@@ -15,7 +13,6 @@ public class Main {
             for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
                 intArray[i] = intArray[i - 1];
             }
-
             intArray[i] = newElement;
         }
 
